535119b1ef827dfe60a01a827442fa6136b1446b
Max Blau <max.blau@gmail.com>
2018-10-04 19:50:28 -0700

    Example of pyautogui

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import time
 import mss
 import numpy
+import matplotlib.pyplot as plt
 
+# Screenshot stress test
 def screen_record_efficient():
     # 800x600 windowed mode
-    mon = {"top": 40, "left": 0, "width": 800, "height": 640}
+    mon = {"top": 0, "left": 0, "width": 320, "height": 240}
 
     title = "[MSS] FPS benchmark"
     fps = 0
@@ -14,7 +16,13 @@ def screen_record_efficient():
     while time.time() - last_time < 1:
         img = numpy.asarray(sct.grab(mon))
         fps += 1
-
     return fps
 
 print("MSS:", screen_record_efficient())
+
+
+# pip install pyautogui to get keyboard output library
+# run this command to press a key:
+
+import pyautogui as pg
+pg.press("a")
