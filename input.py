64acbe839480973e1a4005c77bb7df60c3e17135
Max Blau <mbblau423@gmail.com>
2018-11-01 22:33:58 -0700

    Changed up input a bit to be cleaner on enter and exit.|

--- input.py
+++ input.py
@@ -5,26 +5,50 @@ import sys
 import os
 
 
+# takes a button and presses then releases it
 def p_and_r(button):
-    pcmd = "echo \"PRESS " + button + "\" > ~/.local/share/dolphin-emu/Pipes/pipe"
-    rcmd = "echo \"RELEASE " + button + "\" > ~/.local/share/dolphin-emu/Pipes/pipe"
-    exec_action(pcmd, rcmd, 60)
+    press(button)
+    release(button)
 
+# presses a button
+def press(button):
+    pcmd = "echo \"PRESS " + button + "\" > ~/.local/share/dolphin-emu/Pipes/pipe"
+    exec_action(pcmd, 60)
+
+# releases a button
+def release(button):
+    rcmd = "echo \"RELEASE " + button + "\" > ~/.local/share/dolphin-emu/Pipes/pipe"
+    exec_action(rcmd, 60)
+
+# sets a stick to (x, y)
+# (0, 0) is the top left direction
+# (1, 1) is the bottom right
 def set_stick(stick, x, y):
     scmd = "echo \"SET " + stick + " " + str(x) + " " + str(y) + "\" > ~/.local/share/dolphin-emu/Pipes/pipe"
     os.system(scmd)    
 
+# sets stick back to (0.5, 0.5) the center position
 def reset_stick(stick):
     uscmd = "echo \"SET " + stick + " 0.5 0.5\" > ~/.local/share/dolphin-emu/Pipes/pipe"
     os.system(uscmd)
 
-def exec_action(startcmd, endcmd, lim):
-    os.system(startcmd)
+# execs a start action (like PRESS) then waits a frame
+def exec_action(cmd, lim):
+    os.system(cmd)
     time.sleep(1/lim)
-    os.system(endcmd)
+
+# Cleans up the pipe
+def make_clean(button_arr):
+    reset_stick("MAIN")
+    for but in button_arr:
+        release(but)
 
 def start_game_from_char_select():
     button_arr = ["A", "B", "Z", "L", "R", "Y", "X"]
+
+    # Clean up the pipe so no previous inputs are in it
+    # Only necessary if a script crashed for some reason
+    make_clean(button_arr)
 
     # Move to and Select Falco
     set_stick("MAIN", 0.5, 0)
@@ -44,7 +68,7 @@ def start_game_from_char_select():
     reset_stick("MAIN")
     p_and_r("A")
     set_stick("MAIN", 1, 0.5)
-    time.sleep(8/60)
+    time.sleep(9/60)
     reset_stick("MAIN")
     p_and_r("A")
 
@@ -62,12 +86,20 @@ def start_game_from_char_select():
     time.sleep(3)
 
     start_time = time.time()
-    while(time.time() - start_time < 120):
-        ri = np.random.randint(0, 7)
-        p_and_r(button_arr[ri])
-        rmone = np.random.randint(0, 4)
-        rmtwo = np.random.randint(0, 4)
-        set_stick("MAIN", rmone / 4, rmtwo / 4)   
-        time.sleep(1/60)
+    frames = 0
+    ri = 0
+    while(time.time() - start_time < 126):
+        rmone = np.random.uniform()
+        rmtwo = np.random.uniform()
+        set_stick("MAIN", rmone, rmtwo)
+        if(frames % 2 == 1):
+            ri = np.random.randint(0, 7)
+            press(button_arr[ri])
+        else:
+            release(button_arr[ri])
+        frames = frames + 1
+
+    # Clean the pipe by reseting the stick and release everything
+    make_clean(button_arr)
 
 start_game_from_char_select()
